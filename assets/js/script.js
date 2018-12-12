c94d9aa0d1702b9dc64df1ee6d708af42aaa4dda
travel287623 <45782334+travel287623@users.noreply.github.com>
2018-12-12 09:07:02 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+    $("#btn-callback-carousel1").on("click", function(){
+      $("#callback-modal").modal('show');
+    });
+
+    $("#btn-callback-carousel2").on("click", function(){
+        $("#callback-modal").modal('show');
+    });
+
+    $("#btn-callback-carousel3").on("click", function(){
+        $("#callback-modal").modal('show');
+    });
+
+    $("#btn-callback-carousel4").on("click", function(){
+        $("#callback-modal").modal('show');
+    });
+    
+    $("#btn-callback-phone-icon").on("click", function(){
+      $("#callback-modal").modal('show');
+      $("#inputSite").val('Клиент заказал обратный звонок');
+    });
+
+    $("#btn-callback-navbar").on("click", function(){
+        $("#callback-modal").modal('show');
+        $("#inputSite").val('Клиент заказал обратный звонок');
+    });
+
+    $("#btn-callback-footer").on("click", function(){
+        $("#callback-modal").modal('show');
+        $("#inputSite").val('Клиент заказал обратный звонок');
+    });
+
+    $("#btn-open-franch").on("click", function(){
+        $("#callback-modal3").modal('show');
+    });
+
+    $("#btn-open-franch2").on("click", function(){
+        $("#callback-modal3").modal('show');
+    });
+
+    $("#btn-open-franch3").on("click", function(){
+        $("#callback-modal3").modal('show');
+    });
+
+    $("#btn-open-franch4").on("click", function(){
+        $("#callback-modal3").modal('show');
+    });
+
+    $("#btn-open-franch5").on("click", function(){
+        $("#callback-modal3").modal('show');
+    });
+
+    $("#btn-open-franch6").on("click", function(){
+        $("#callback-modal3").modal('show');
+    });
+
+    $("#btn-open-franch7").on("click", function(){
+        $("#callback-modal3").modal('show');
+    });
+
+    $("#btn-transport").on("click", function(){
+        $("#callback-modal4").modal('show');
+    });
+
+    $("#btn-callback-carousel-ok").on("click", function(){
+      $("#callback-modal").modal('hide');
+    });
+
+    $("#callBackMain").on("click", function(){
+      $("#callback-modal").modal('hide');
+    });
+
+    $("#success-modal-ok").on("click", function(){
+      $("#success-modal").modal('hide');
+    });
+
+    window.setInterval(function() {
+      function getDateNow() {
+        var n =  new Date();
+        var y = n.getFullYear();
+        var m = n.getMonth() + 1;
+        m = m.toString().length < 2 ? '0'+m : m;
+        var d = n.getDate();
+        d = d.toString().length < 2 ? '0'+d : d;
+        var h = n.getHours();
+        h = h.toString().length < 2 ? '0'+h : h;
+        var min = n.getMinutes();
+        min = min.toString().length < 2 ? '0'+min : min;
+        $('#inputTime').val(d + '-' + m + '-' + y + ' ' + h + ':' + min);
+        $('#inputTime2').val(d + '-' + m + '-' + y + ' ' + h + ':' + min);
+        $('#inputTime3').val(d + '-' + m + '-' + y + ' ' + h + ':' + min);
+        $('#inputTime4').val(d + '-' + m + '-' + y + ' ' + h + ':' + min);
+      };
+      getDateNow();
+    }, 5000);
+  });
+
+  $(document.body).on('hide.bs.modal,hidden.bs.modal', function () {
+    $('body').css('padding-right','0');
+  });
